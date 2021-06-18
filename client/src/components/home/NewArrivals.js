0c3d8b3650321bd1e9d02aca841bd9d2b99621bf
timothy <timothy9369@gmail.com>
2021-06-18 14:06:50 +0300

    make it better

---
+++ client/src/components/home/NewArrivals.js
@@ -0,0 +1,59 @@
+import React, {useState, useEffect, useCallback} from 'react';
+import Grid from '@material-ui/core/Grid';
+import {Pagination, Col, Row } from 'antd';
+import ProductCard from '../cards/ProductCard';
+import { getProducts, getProductsCount } from '../../functions/product';
+import LoadingCard from '../cards/LoadingCard';
+
+
+const NewArrivals = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [productsCount, setProductsCount] = useState(0);
+    const [page, setPage] = useState(1);
+
+    const loadProducts = useCallback(() => {
+        setLoading(true)
+        getProducts('createdAt', 'desc', page).then(res => {
+            setLoading(false);
+            setProducts(res.data.products);
+        }).catch(err => {
+            setLoading(false);
+            console.log(err)
+        })
+    }, [page])
+
+    useEffect(() => {
+        loadProducts();
+    }, [page, loadProducts]);
+
+    useEffect(() => {
+        getProductsCount().then(res => {
+            setProductsCount(res.data.total)
+        }).catch(err => {
+            console.log(err)
+        })
+    }, [])
+
+    //SORT, ORDER, LIMIT
+
+
+
+    return (
+        <>
+            {loading ? (<LoadingCard count={3} />) :
+            (<Grid container spacing={2} style={{marginLeft: '1rem', marginRight: '2rem'}} justify="space-around" wrap="wrap">
+                {products.length > 0 && products.map(product => (
+                    <ProductCard key={product._id} product={product} />
+                ))}
+            </Grid>)}
+            <Row style={{padding: '2rem 0'}} justify="center">
+                <Col span={12} offset={6}>
+                    <Pagination current={page} total={(productsCount/3) * 10} onChange={val => setPage(val)} />
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default NewArrivals
